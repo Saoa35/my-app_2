57889d6c86b5b8d5928341c59b7d1e9f4e3165a0
Saoa35 <saoamah@gmail.com>
2022-08-31 15:14:25 +0400

    created Exampales component

---
+++ src/components/Exampales/Exampales.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Exampales extends React.Component {
+
+    render() {
+
+        return(
+            <div>
+                <h2>Exampales</h2>
+            </div>
+        )
+    }
+}
+
+export default Exampales;
