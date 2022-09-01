820880799a2593e09b41ca8df287ccd25d7ed81a
Saoa35 <saoamah@gmail.com>
2022-09-01 13:45:50 +0400

    added StatefullExample to App js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 // import Exampales from '../Exampales/Exampales';
+import StatefullExample from '../StatefullExample/StatefullExample';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <StatefullExample/>
     </div>
   );
 }
---
+++ src/components/StatefullExample/StatefullExample.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class StatefullExample extends React.Component {
+
+    render() {
+
+        return(
+
+        )
+    }
+}
+
+export default StatefullExample;
