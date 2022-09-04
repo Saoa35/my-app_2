06a5cfcc2444b4387e5eb5f1a2049c7df2f5c4b8
Saoa35 <saoamah@gmail.com>
2022-09-04 21:11:52 +0200

    changed App component from functional to class

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,19 @@
 // import Exampales from '../Exampales/Exampales';
 // import StatefullExample from '../StatefullExample/StatefullExample';
 import LifecycleExample from '../LifecycleExample/LifecycleExample';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <LifecycleExample/>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+
+    return (
+      <div className="App">
+        <LifecycleExample/>
+      </div>
+    );
+  }
 }
 
 export default App;
