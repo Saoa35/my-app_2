acbfae12416f9a1dd0f3ae567f0b9d6224fa3e9f
Saoa35 <saoamah@gmail.com>
2022-09-04 20:48:24 +0200

    created Item component

--- src/components/LifecycleExample/LifecycleExample.js
+++ src/components/LifecycleExample/LifecycleExample.js
@@ -1,6 +1,25 @@
 import React from "react";
 
+
+const Item =({ item }) => {
+
+    const { title } = item;
+
+    return (
+        <div>
+            <h3>
+                {title}
+            </h3>
+        </div>
+    )
+}
+
 class LifecycleExample extends React.Component {
+
+
+    state = {
+        data: []
+    }
 
     componentDidMount() {
         // console.log('componentDidMount');
@@ -10,17 +29,24 @@ class LifecycleExample extends React.Component {
         fetch(url)
             .then(resp => resp.json())
             .then(data => {
-                console.log(data);
-            })
+                // console.log(data);
+
+                this.setState({
+                    data:data
+                })
+            });
     }
 
     render() {
 
-        console.log('first render');
+        // console.log('first render');
+        // console.log(this.state.data);
 
         return(
             <div>
                 <h3>LifecycleExample</h3>
+
+
             </div>
         )
     }
