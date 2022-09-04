b243cfb6621136582738b7de6ac7aed74a1807f1
Saoa35 <saoamah@gmail.com>
2022-09-04 22:12:11 +0200

    created store in redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import { legacy_createStore } from 'redux';
 
 
 const initialState = 2;
@@ -12,7 +13,7 @@ const reducer = (state = initialState, action) => {
   } else return state;
 }
 
-
+const store = legacy_createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
