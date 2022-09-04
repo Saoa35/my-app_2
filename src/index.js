34b0d23ee8989f06eb12096c610627cc0f92342c
Saoa35 <saoamah@gmail.com>
2022-09-04 22:16:40 +0200

    created Provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import { legacy_createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 
 const initialState = 2;
@@ -18,6 +19,8 @@ const store = legacy_createStore(reducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>
 );
