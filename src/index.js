017d07205a9b8b1f0b64d0e1097b601d7befd51f
Saoa35 <saoamah@gmail.com>
2022-09-05 12:37:36 +0200

    + DEC case

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ const reducer = (state = initialState, action) => {
   switch(action.type) {
     case 'INC':
       return state + 1;
+      case 'DEC':
+      return state - 1;
       default:
         return state;
   }
